3883736f8bf9cb4d0f04f34c9f21a241e798d634
fenil316 <fenil316@localhost>
2018-07-05 18:31:01 -0400

    springboot: test changes

--- src/main/java/io/egen/controller/EmployeeController.java
+++ src/main/java/io/egen/controller/EmployeeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/employees")
+@RequestMapping(value = "/employee")
 public class EmployeeController {
 
     @Autowired
